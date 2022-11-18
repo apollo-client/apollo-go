f282a2b516599c4723b8f39fcf8144d903138733
xnzone <60599181+xnzone@users.noreply.github.com>
2022-11-18 07:34:03 +0000

    update auth

--- auth/auth.go
+++ auth/auth.go
@@ -3,3 +3,11 @@ package auth
 type Auth interface {
 	Headers(uri string, appID string, secret string) map[string][]string
 }
+
+var (
+	DefaultAuth = newApolloAuth()
+)
+
+func newApolloAuth() *ApolloAuth {
+	return &ApolloAuth{}
+}
